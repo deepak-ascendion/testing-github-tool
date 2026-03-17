303441846500cccf59e38dc4ed2823db19efb615
deepak-ascendion <deepak.suresh@ascendion.com>
2026-03-17 17:19:51 +0530

    Added HCF and LCM functionality for 4 numbers while preserving existing code

--- auto_scripts/AddNumbers.java
+++ auto_scripts/AddNumbers.java
@@ -1,24 +1,20 @@
 public class AddNumbers {
     
-    // Existing method to add two numbers
+    // Existing functionality - basic addition
     public int add(int a, int b) {
         return a + b;
     }
     
-    // Existing method to add three numbers
     public int add(int a, int b, int c) {
         return a + b + c;
     }
     
-    // Existing method to add four numbers
     public int add(int a, int b, int c, int d) {
         return a + b + c + d;
     }
     
-    // NEW: Method to find GCD/HCF of two numbers using Euclidean algorithm
-    public int gcd(int a, int b) {
-        a = Math.abs(a);
-        b = Math.abs(b);
+    // New HCF functionality for 4 numbers
+    public int hcf(int a, int b) {
         while (b != 0) {
             int temp = b;
             b = a % b;
@@ -27,40 +23,44 @@ public class AddNumbers {
         return a;
     }
     
-    // NEW: Method to find HCF of three numbers
     public int hcf(int a, int b, int c) {
-        return gcd(gcd(a, b), c);
+        return hcf(hcf(a, b), c);
     }
     
-    // NEW: Method to find HCF of four numbers
     public int hcf(int a, int b, int c, int d) {
-        int hcfAB = gcd(a, b);
-        int hcfABC = gcd(hcfAB, c);
-        return gcd(hcfABC, d);
+        return hcf(hcf(hcf(a, b), c), d);
     }
     
-    // NEW: Method to find LCM of two numbers
+    // LCM functionality (using HCF)
     public int lcm(int a, int b) {
-        if (a == 0 || b == 0) {
-            return 0;
-        }
-        return Math.abs(a * b) / gcd(a, b);
+        return Math.abs(a * b) / hcf(a, b);
     }
     
-    // NEW: Method to find LCM of three numbers
     public int lcm(int a, int b, int c) {
         return lcm(lcm(a, b), c);
     }
     
-    // NEW: Method to find LCM of four numbers
     public int lcm(int a, int b, int c, int d) {
         return lcm(lcm(lcm(a, b), c), d);
     }
     
+    // Main method for testing
     public static void main(String[] args) {
         AddNumbers calculator = new AddNumbers();
+        
+        // Test addition
         System.out.println("Addition of 10, 20: " + calculator.add(10, 20));
+        System.out.println("Addition of 10, 20, 30: " + calculator.add(10, 20, 30));
+        System.out.println("Addition of 10, 20, 30, 40: " + calculator.add(10, 20, 30, 40));
+        
+        // Test HCF
+        System.out.println("HCF of 12, 18: " + calculator.hcf(12, 18));
+        System.out.println("HCF of 12, 18, 24: " + calculator.hcf(12, 18, 24));
         System.out.println("HCF of 12, 18, 24, 36: " + calculator.hcf(12, 18, 24, 36));
+        
+        // Test LCM
+        System.out.println("LCM of 4, 6: " + calculator.lcm(4, 6));
+        System.out.println("LCM of 4, 6, 8: " + calculator.lcm(4, 6, 8));
         System.out.println("LCM of 4, 6, 8, 12: " + calculator.lcm(4, 6, 8, 12));
     }
 }

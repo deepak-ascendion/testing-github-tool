210e4d62c02e8a82f47416d35d5648da10e80229
deepak-ascendion <deepak.suresh@ascendion.com>
2026-03-17 16:46:39 +0530

    Added HCF and LCM functionality for multiple numbers including 4 numbers support

--- auto_scripts/AddNumbers.java
+++ auto_scripts/AddNumbers.java
@@ -1,68 +1,66 @@
 public class AddNumbers {
     
-    // Method to add two numbers
+    // Existing method to add two numbers
     public int add(int a, int b) {
         return a + b;
     }
     
-    // Method to add three numbers
+    // Existing method to add three numbers
     public int add(int a, int b, int c) {
         return a + b + c;
     }
     
-    // Method to add four numbers
+    // Existing method to add four numbers
     public int add(int a, int b, int c, int d) {
         return a + b + c + d;
     }
     
-    // Method to find GCD/HCF of two numbers
+    // NEW: Method to find GCD/HCF of two numbers using Euclidean algorithm
     public int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
+        a = Math.abs(a);
+        b = Math.abs(b);
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
-        return gcd(b, a % b);
+        return a;
     }
     
-    // Method to find HCF of three numbers
+    // NEW: Method to find HCF of three numbers
     public int hcf(int a, int b, int c) {
         return gcd(gcd(a, b), c);
     }
     
-    // Method to find HCF of four numbers
+    // NEW: Method to find HCF of four numbers
     public int hcf(int a, int b, int c, int d) {
-        return gcd(gcd(gcd(a, b), c), d);
+        int hcfAB = gcd(a, b);
+        int hcfABC = gcd(hcfAB, c);
+        return gcd(hcfABC, d);
     }
     
-    // Method to find LCM of two numbers
+    // NEW: Method to find LCM of two numbers
     public int lcm(int a, int b) {
-        return (a * b) / gcd(a, b);
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        return Math.abs(a * b) / gcd(a, b);
     }
     
-    // Method to find LCM of three numbers
+    // NEW: Method to find LCM of three numbers
     public int lcm(int a, int b, int c) {
         return lcm(lcm(a, b), c);
     }
     
-    // Method to find LCM of four numbers
+    // NEW: Method to find LCM of four numbers
     public int lcm(int a, int b, int c, int d) {
         return lcm(lcm(lcm(a, b), c), d);
     }
     
-    // Main method for testing
     public static void main(String[] args) {
         AddNumbers calculator = new AddNumbers();
-        
-        // Test addition
         System.out.println("Addition of 10, 20: " + calculator.add(10, 20));
-        System.out.println("Addition of 10, 20, 30: " + calculator.add(10, 20, 30));
-        System.out.println("Addition of 10, 20, 30, 40: " + calculator.add(10, 20, 30, 40));
-        
-        // Test HCF
-        System.out.println("HCF of 12, 18, 24: " + calculator.hcf(12, 18, 24));
         System.out.println("HCF of 12, 18, 24, 36: " + calculator.hcf(12, 18, 24, 36));
-        
-        // Test LCM
-        System.out.println("LCM of 4, 6, 8: " + calculator.lcm(4, 6, 8));
         System.out.println("LCM of 4, 6, 8, 12: " + calculator.lcm(4, 6, 8, 12));
     }
 }

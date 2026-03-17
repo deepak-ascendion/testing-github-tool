d23cd964f555436171183cb99e3c73683e1b5d55
deepak-ascendion <deepak.suresh@ascendion.com>
2026-03-17 21:54:58 +0530

    Add string reversal methods to AddNumbers.java

--- auto_scripts/AddNumbers.java
+++ auto_scripts/AddNumbers.java
@@ -1,64 +1,52 @@
 public class AddNumbers {
     
-    // Existing functionality - basic addition
-    public static int add(int a, int b) {
+    // Method to add two numbers
+    public int addTwoNumbers(int a, int b) {
         return a + b;
     }
     
-    public static int add(int a, int b, int c) {
-        return a + b + c;
-    }
-    
-    public static int add(int a, int b, int c, int d) {
-        return a + b + c + d;
-    }
-    
-    // New HCF (Highest Common Factor) functionality for 4 numbers
-    public static int hcf(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
+    // Method to add multiple numbers
+    public int addMultipleNumbers(int... numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
         }
-        return Math.abs(a);
+        return sum;
     }
     
-    public static int hcf(int a, int b, int c) {
-        return hcf(hcf(a, b), c);
+    // Method to reverse a string
+    public String reverseString(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+        
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+        return reversed.toString();
     }
     
-    public static int hcf(int a, int b, int c, int d) {
-        return hcf(hcf(hcf(a, b), c), d);
-    }
-    
-    // LCM (Least Common Multiple) functionality for completeness
-    public static int lcm(int a, int b) {
-        return Math.abs(a * b) / hcf(a, b);
-    }
-    
-    public static int lcm(int a, int b, int c) {
-        return lcm(lcm(a, b), c);
-    }
-    
-    public static int lcm(int a, int b, int c, int d) {
-        return lcm(lcm(lcm(a, b), c), d);
+    // Alternative method to reverse a string using StringBuilder
+    public String reverseStringAlternative(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+        return new StringBuilder(input).reverse().toString();
     }
     
     // Main method for testing
     public static void main(String[] args) {
-        // Test addition
-        System.out.println("Addition of 2 numbers (5, 3): " + add(5, 3));
-        System.out.println("Addition of 3 numbers (5, 3, 2): " + add(5, 3, 2));
-        System.out.println("Addition of 4 numbers (5, 3, 2, 1): " + add(5, 3, 2, 1));
+        AddNumbers calculator = new AddNumbers();
         
-        // Test HCF
-        System.out.println("HCF of 2 numbers (12, 8): " + hcf(12, 8));
-        System.out.println("HCF of 3 numbers (12, 8, 4): " + hcf(12, 8, 4));
-        System.out.println("HCF of 4 numbers (12, 8, 4, 16): " + hcf(12, 8, 4, 16));
+        // Test addition methods
+        System.out.println("Adding 5 + 3: " + calculator.addTwoNumbers(5, 3));
+        System.out.println("Adding 1, 2, 3, 4, 5: " + calculator.addMultipleNumbers(1, 2, 3, 4, 5));
         
-        // Test LCM
-        System.out.println("LCM of 2 numbers (4, 6): " + lcm(4, 6));
-        System.out.println("LCM of 3 numbers (4, 6, 8): " + lcm(4, 6, 8));
-        System.out.println("LCM of 4 numbers (4, 6, 8, 12): " + lcm(4, 6, 8, 12));
+        // Test string reversal methods
+        String testString = "Hello World";
+        System.out.println("Original string: " + testString);
+        System.out.println("Reversed string: " + calculator.reverseString(testString));
+        System.out.println("Reversed string (alternative): " + calculator.reverseStringAlternative(testString));
     }
 }

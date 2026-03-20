106f37d125ac999dae5053f608609784e557814b
deepak-ascendion <github-writer-agent@example.com>
2026-03-20 12:14:17 +0530

    Add Java test to add 20 and 4 in test folder

---
+++ test/AddTwoNumbersTest.java
@@ -0,0 +1,8 @@
+public class AddTwoNumbersTest {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 4;
+        int sum = a + b;
+        System.out.println("Sum of 20 and 4 is: " + sum);
+    }
+}

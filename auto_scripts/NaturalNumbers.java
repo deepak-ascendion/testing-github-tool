29701e7cb0393409ff1e04d3385d974fc7f3e444
deepak-ascendion <github-actions@github.com>
2026-04-23 20:33:03 +0530

    Add Java program to print N natural numbers

--- auto_scripts/NaturalNumbers.java
+++ auto_scripts/NaturalNumbers.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class NaturalNumbers {
-    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
@@ -15,40 +14,11 @@ public class NaturalNumbers {
         
         System.out.println("First " + n + " natural numbers are:");
         
-        // Method 1: Using for loop
-        printUsingForLoop(n);
-        
-        System.out.println();
-        
-        // Method 2: Using while loop
-        printUsingWhileLoop(n);
-        
-        scanner.close();
-    }
-    
-    /**
-     * Print natural numbers using for loop
-     * @param n - number of natural numbers to print
-     */
-    public static void printUsingForLoop(int n) {
-        System.out.println("Using For Loop:");
         for (int i = 1; i <= n; i++) {
             System.out.print(i + " ");
         }
-        System.out.println();
-    }
-    
-    /**
-     * Print natural numbers using while loop
-     * @param n - number of natural numbers to print
-     */
-    public static void printUsingWhileLoop(int n) {
-        System.out.println("Using While Loop:");
-        int i = 1;
-        while (i <= n) {
-            System.out.print(i + " ");
-            i++;
-        }
-        System.out.println();
+        
+        System.out.println(); // New line after printing all numbers
+        scanner.close();
     }
 }

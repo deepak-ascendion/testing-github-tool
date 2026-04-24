e2cc7868e98c4fb3e2456ae62eb214c41e929893
deepak-ascendion <github-agent@example.com>
2026-04-24 14:38:54 +0530

    Add Java code to calculate sum of 20 and 30

--- auto_scripts/AddNumbers.java
+++ auto_scripts/AddNumbers.java
@@ -2,7 +2,7 @@ public class AddNumbers {
     public static void main(String[] args) {
         // Define two numbers
         int num1 = 20;
-        int num2 = 5;
+        int num2 = 30;
         
         // Add the numbers
         int sum = num1 + num2;
@@ -10,6 +10,6 @@ public class AddNumbers {
         // Display the result
         System.out.println("First number: " + num1);
         System.out.println("Second number: " + num2);
-        System.out.println("Sum: " + sum);
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }

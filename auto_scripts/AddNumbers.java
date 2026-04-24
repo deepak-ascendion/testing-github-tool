412e90d3d4a0e705a754ad9e82826d2255abdc91
deepak-ascendion <github-actions@github.com>
2026-04-24 14:53:00 +0530

    Add multiplication function to multiply 2 numbers (2 and 10) in AddNumbers.java

--- auto_scripts/AddNumbers.java
+++ auto_scripts/AddNumbers.java
@@ -1,15 +1,36 @@
 public class AddNumbers {
+    
+    // Existing addition functionality
+    public static int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+    
+    // Method to add multiple numbers
+    public static int addNumbers(int... numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+    
+    // New multiplication function as requested
+    public static int multiplyTwoNumbers(int a, int b) {
+        return a * b;
+    }
+    
+    // Specific method to multiply 2 and 10 as requested
+    public static int multiplyTwoAndTen() {
+        return multiplyTwoNumbers(2, 10);
+    }
+    
     public static void main(String[] args) {
-        // Define two numbers
-        int num1 = 20;
-        int num2 = 30;
+        // Test addition functions
+        System.out.println("Addition of 5 and 3: " + addTwoNumbers(5, 3));
+        System.out.println("Addition of multiple numbers (1,2,3,4,5): " + addNumbers(1, 2, 3, 4, 5));
         
-        // Add the numbers
-        int sum = num1 + num2;
-        
-        // Display the result
-        System.out.println("First number: " + num1);
-        System.out.println("Second number: " + num2);
-        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+        // Test multiplication functions
+        System.out.println("Multiplication of 2 and 10: " + multiplyTwoNumbers(2, 10));
+        System.out.println("Specific multiplication of 2 and 10: " + multiplyTwoAndTen());
     }
 }
